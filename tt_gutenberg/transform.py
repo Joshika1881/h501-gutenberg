ffc67163edfe1f7ce9b6dbbbbf4ed39bc0c28f81
Joshika Sathyamathan <jsathya@iu.edu>
2026-09-25 14:47:12 -0400

    Add DATA path for Gutenberg datasets

--- tt_gutenberg/transform.py
+++ tt_gutenberg/transform.py
@@ -1,21 +1,19 @@
-import pandas as pd
+from pandas import read_csv, merge
+
+
+DATA = (
+    "https://raw.githubusercontent.com/rfordatascience/tidytuesday/"
+    "main/data/2025/2025-06-03/"
+)
 
 
 def get_data():
     """Load and merge the Gutenberg authors and metadata datasets."""
-    authors_url = (
-        "https://raw.githubusercontent.com/rfordatascience/tidytuesday/"
-        "main/data/2025/2025-06-03/gutenberg_authors.csv"
-    )
-    metadata_url = (
-        "https://raw.githubusercontent.com/rfordatascience/tidytuesday/"
-        "main/data/2025/2025-06-03/gutenberg_metadata.csv"
-    )
+    authors = read_csv(DATA + "gutenberg_authors.csv")
+    metadata = read_csv(DATA + "gutenberg_metadata.csv")
 
-    authors = pd.read_csv(authors_url)
-    metadata = pd.read_csv(metadata_url)
-
-    data = authors.merge(
+    data = merge(
+        authors,
         metadata,
         on="gutenberg_author_id",
         suffixes=("_author", "_metadata"),
